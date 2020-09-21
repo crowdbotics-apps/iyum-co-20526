e5a9a9d337c4e9c5ffc7dae57dbffd7ee8572244
Crowdbotics <team@crowdbotics.com>
2020-09-21 19:57:30 +0000

    Update models

--- backend/iyum_co_20526/wsgi.py
+++ backend/iyum_co_20526/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iyum_co_20526.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iyum_co_20526.settings")
 
 application = get_wsgi_application()
