a8e2d8e7c0630255e8d6203762c4437fe3553a83
Crowdbotics <team@crowdbotics.com>
2020-09-21 19:55:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/iyum_co_20526/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iyum_co_20526;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "iyum_co_20526";
+    }
+}
